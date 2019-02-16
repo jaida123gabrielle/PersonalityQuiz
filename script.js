5cab9e872844cc89402a11bd77fe04119c63c7f3
jaida <jaidao@csedge.org>
2019-02-16 15:48:16 -0800

    i got ahead on my personality quiz

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let hi = "hello"
-console.log(hi);
+let submitButton = document.getElementById("submit-buttom")
+submitButton.addEventListener("click", processResults)
+function processResults(){
+  console.log ("submitButtonPushed")
+}
+
+let resetButton = document.getElementById("reset-button")
+resetButton.addEventListener("click", resetQuiz)

61cf0397b688b748e2166827e3f4f9226ef9935f
jaida <jaidao@csedge.org>
2019-03-17 16:34:39 -0700

    ok

--- script.js
+++ script.js
@@ -3,6 +3,3 @@ submitButton.addEventListener("click", processResults)
 function processResults(){
   console.log ("submitButtonPushed")
 }
-
-let resetButton = document.getElementById("reset-button")
-resetButton.addEventListener("click", resetQuiz)
